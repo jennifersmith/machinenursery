c9aadea184a1948ba1227b18dac9f3ee234f0f3a
Mark Needham <m.h.needham@gmail.com>
2012-12-26 14:38:04 +0000

    mark: tweaking heap/output

--- build-forest.sh
+++ build-forest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #lein run -m main.java.MahoutPlaybox
-java -cp target/machinenursery-1.0.0-SNAPSHOT-standalone.jar main.java.MahoutPlaybox
+java -Xmx1024m -cp target/machinenursery-1.0.0-SNAPSHOT-standalone.jar main.java.MahoutPlaybox
--- parallel-forests.sh
+++ parallel-forests.sh
@@ -1,11 +1,14 @@
 #!/bin/bash 
 
 start=`date`
+startTime=`date '+%s'`
 numberOfRuns=$1
 
 seq 1 ${numberOfRuns} | parallel -P 8 "./build-forest.sh"
 
 end=`date`
+endTime=`date '+%s'`
 
 echo "Started: ${start}"
 echo "Finished: ${end}"
+echo "Took: " $(expr $endTime - $startTime)
